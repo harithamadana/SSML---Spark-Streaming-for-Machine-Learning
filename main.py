c51e1cead832300e27136e1cd9b2007080eea938
sowhith <sowhithreddy3@gmail.com>
2021-11-25 22:13:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import json
 import pandas as pd
 TCP_IP = "localhost"
 TCP_PORT = 6100
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    s.connect((TCP_IP, TCP_PORT))
+    soc.connect((TCP_IP, TCP_PORT))
     while True:
-        json_recv=s.recv(2048).decode()
+        json_recv=soc.recv(2048).decode()
         a_json=json.loads(json_recv)
         df=pd.DataFrame.from_dict(a_json,orient="index")
         print(df)
