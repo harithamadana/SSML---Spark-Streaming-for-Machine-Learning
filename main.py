422440eca96c8f07d44751f2d10e7593c1d97fa4
Haritha Madana <60546642+harithamadana@users.noreply.github.com>
2021-12-04 07:25:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 TCP_IP = "localhost"
 TCP_PORT = 6100
 soc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 try:
     soc.connect((TCP_IP, TCP_PORT))
     while True:
@@ -12,6 +13,7 @@ try:
         df=pd.DataFrame.from_dict(a_json,orient="index")
         print(df)
         print('.......................................................................................................')
+        
 except Exception as e:
     print(e)
 
