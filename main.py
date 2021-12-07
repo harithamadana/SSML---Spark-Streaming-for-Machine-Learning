9563f0bd3c1b881525306b2830420683cac6cbc4
sowhith <sowhithreddy3@gmail.com>
2021-12-07 07:53:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 import socket
 import json
 import pandas as pd
-from pyspark.sql import SparkSession
-from pyspark.ml.feature import StopWordsRemover
+TCP_IP = "localhost"
+TCP_PORT = 6100
+soc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def prepro(df):             # used for splitting string to individual words in a list and remove username,urls and numbers
+try:
+    soc.connect((TCP_IP, TCP_PORT))
+    while True:
+        json_recv=soc.recv(2048).decode()
+        a_json=json.loads(json_recv)
+        df=pd.DataFrame.from_dict(a_json,orient="index")
+        prepro(df)
+        break
+        print('.......................................................................................................')
+except Exception as e:
+    print(e)
+
+
+# In[1]:
+
+
+def prepro(df):
     for i in range(0,len(df['feature1'])):
         clean=list(df["feature1"][i].split(" "))
-        #print(clean)
+        print(clean)
         for j in clean:
             if(j.startswith("@")):
                 clean.remove(j)
@@ -23,27 +46,64 @@ def prepro(df):             # used for splitting string to individual words in a
                 clean.remove(j)
                 continue
         df['feature1'][i] = clean
-        #print(clean)
-    
-TCP_IP = "localhost"
-TCP_PORT = 6100
-soc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(clean)
 
-try:
-    soc.connect((TCP_IP, TCP_PORT))
-    while True:
-        json_recv=soc.recv(2048).decode()
-        a_json=json.loads(json_recv)
-        df=pd.DataFrame.from_dict(a_json,orient="index")
-        prepro(df)
-        
-        spark = SparkSession.builder.appName("pandas to spark").getOrCreate()
-        df_spark = spark.createDataFrame(df)       #Convert pandas data frame to spark df
-        
 
-        without_stop = StopWordsRemover(inputCol="feature1", outputCol="filtered")
-        without_stop.transform(df_spark).show(truncate=False)       #Results in the removal of stop words 
-        print('.......................................................................................................')
-        
-except Exception as e:
-    print(e)
+# In[3]:
+
+
+df
+
+
+# In[4]:
+
+
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName(
+  "pandas to spark").getOrCreate()
+
+
+# In[5]:
+
+
+df_spark = spark.createDataFrame(df)
+
+
+# In[6]:
+
+
+df_spark.show()
+
+
+# In[71]:
+
+
+from pyspark.ml.feature import StopWordsRemover
+
+without_stop = StopWordsRemover(inputCol="feature1", outputCol="filtered")
+without_stop.transform(df_spark).show()
+
+
+# In[87]:
+
+
+from pyspark.ml.feature import CountVectorizer
+# fit a CountVectorizerModel from the corpus.
+cv = CountVectorizer(inputCol="filtered", outputCol="final", vocabSize=1000, minDF=2.0)
+model = cv.fit(without_stop.transform(df_spark))
+
+result = model.transform(without_stop.transform(df_spark))
+# result.show(truncate=False)
+
+
+# In[88]:
+
+
+result.show()
+
+
+# In[ ]:
+
+
+
+
